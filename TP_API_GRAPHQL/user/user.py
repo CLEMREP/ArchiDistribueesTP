385ccdf104e075b60e0bce114224aabeb759be19
CLEMREP <contact@clement-repel.fr>
2023-11-19 09:33:05 +0100

    :bug: Fix methods of endpoint

--- TP_API_GRAPHQL/user/user.py
+++ TP_API_GRAPHQL/user/user.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, make_response
+from flask import Flask, jsonify, make_response
 import requests
 import json
 import grpc
@@ -7,23 +7,23 @@ import booking_pb2
 
 app = Flask(__name__)
 
-PORT = 3203
+PORT = 3004
 HOST = '0.0.0.0'
 
-# def get_booking_by_userid(stub, userid):
-#    booking = stub.GetBookingByUserId(booking_pb2.UserId(userid=userid))
-#    return booking
+def get_booking_by_userid(stub, userid):
+   booking = stub.GetBookingByUserId(booking_pb2.UserId(userid=userid))
+   return booking
 
-# def get_list_bookings(stub):
-#    allBookings = stub.GetListBookings(booking_pb2.Empty())
-#    for booking in allBookings.bookings:
-#        print(booking)
+def get_list_bookings(stub):
+   allBookings = stub.GetListBookings(booking_pb2.Empty())
+   for booking in allBookings.bookings:
+       print(booking)
 
-# channel = grpc.insecure_channel('localhost:3002')
-# stub = booking_pb2_grpc.BookingStub(channel)
+channel = grpc.insecure_channel('localhost:3002')
+stub = booking_pb2_grpc.BookingStub(channel)
 
-# get_booking_by_userid(stub, "chris_rivers")
-# get_list_bookings(stub)
+get_booking_by_userid(stub, "chris_rivers")
+get_list_bookings(stub)
 
 with open('{}/databases/users.json'.format("."), "r") as jsf:
     users = json.load(jsf)["users"]
@@ -66,7 +66,7 @@ def get_user_bookings(userid):
     return make_response(jsonify(response))
 
 
-@app.route("/users/add/<userid>/<username>/<lastactive>", methods=['GET'])
+@app.route("/users/add/<userid>/<username>/<lastactive>", methods=['POST'])
 def create_user(userid, username, lastactive):
     for user in users:
         if str(user["id"]) == str(userid):
@@ -86,7 +86,7 @@ def create_user(userid, username, lastactive):
     return make_response(jsonify({"Succes": "User created"}), 200)
 
 
-@app.route("/users/delete/<userid>", methods=['GET'])
+@app.route("/users/delete/<userid>", methods=['DELETE'])
 def delete_user(userid):
 
     # Recherche de l'utilisateur par ID
@@ -103,7 +103,7 @@ def delete_user(userid):
         return make_response(jsonify({"error": "Utilisateur non trouvé"}), 400)
 
 
-@app.route('/users/update/<userid>/<username>/<lastactive>', methods=['GET'])
+@app.route('/users/update/<userid>/<username>/<lastactive>', methods=['PUT'])
 def update_user(userid, username, lastactive):
     # Recherche de l'utilisateur par ID
     user_to_update = next((user for user in users if user['id'] == userid), None)
